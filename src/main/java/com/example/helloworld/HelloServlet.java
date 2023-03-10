7b62c5ff72ccf471f633f72624f2dc38e39ef409
Gina Ozimisa Grimal <65407043+arpkk@users.noreply.github.com>
2023-03-10 18:18:30 -0300

    Update HelloServlet.java

--- src/main/java/com/example/helloworld/HelloServlet.java
+++ src/main/java/com/example/helloworld/HelloServlet.java
@@ -1,15 +1,24 @@
-<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Hello World</title>
-</head>
-<body>
-<h1><%= "prueba de integracion continua con despliegue y commit con vault prueba desde jenkins docker" %>
-</h1>
-    <%String username = application.getInitParameter("username");%>
-    </h2><%= username %><h2>
-<br/>
- <a href="/hello-servlet">Hola tsoft</a>
-</body>
-</html>
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hola!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
